e012ea1abb56b36f8f3e0be2ca2ff7aaaf675eb4
ndhika <azumarafi@gmail.com>
2025-12-06 00:12:02 +0700

    UI Update navbar and input quantity

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -12,7 +12,6 @@ const navLinks = [
 { name: "Dashboard", href: "/dashboard" },
 { name: "Input Data", href: "/input" },
 { name: "Reports", href: "/reports" },
-{ name: "Halo", href: "/reports" },
 ];
 
 export default function Navbar() {
